d9785edd084ea598c97dd14ed99787ee4c4b90c6
Timo Kröger <timokroeger93@gmail.com>
2022-03-19 14:39:29 +0100

    First shot at PIO code

--- src/main.c
+++ src/main.c
@@ -7,10 +7,11 @@
 #include <assert.h>
 #include <stdint.h>
 
-#include "hardware/dma.h"
-#include "pico/stdlib.h"
-#include "read_addr.pio.h"
+#include "cartridge_interface.pio.h"
 #include "config.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
 
 // TODO: Move to QSPI flash
 extern uint8_t g_rom[ROM_SIZE];
@@ -34,7 +35,6 @@ static void init_dma(uint addr_dreq, const volatile void *addr_fifo,
   s_rom_addr = rom_addr;
 
   // Read ROM data DMA chain
-  // TODO: is transfer count modified???
   int dma_fetch_addr = dma_claim_unused_channel(true);
   int dma_addr_offset = dma_claim_unused_channel(true);
   int dma_data_out = dma_claim_unused_channel(true);
@@ -70,25 +70,50 @@ static void init_dma(uint addr_dreq, const volatile void *addr_fifo,
                         1, true);
 }
 
-static void init_cartridge_interface(PIO pio, uint in_pin_base,
-                                     uint out_pin_base, uintptr_t rom_addr) {
+static void init_cartridge_interface(PIO pio, uint muxed_cartridge_signals,
+                                     uint cartridge_signals, uint mux_control,
+                                     uintptr_t rom_addr) {
   uint sm, offset;
+  pio_sm_config c;
+
+  // Configure GPIOs
+  uint temp_sm = pio_claim_unused_sm(pio, true);
+  pio_sm_set_pins(pio, sm, ADDR_HI << mux_control);
+  pio_sm_set_consecutive_pindirs(pio, sm, muxed_cartridge_signals,
+                                 MUXED_SIGNAL_BITS, false);
+  pio_sm_set_consecutive_pindirs(pio, sm, cartridge_signals, SIGNAL_BITS,
+                                 false);
+  pio_sm_set_consecutive_pindirs(pio, sm, mux_control, CONTROL_BITS, true);
 
   // Configure read_addr state machine
-  sm = pio_claim_unused_sm(pio, true);
+  uint read_addr_sm = temp_sm;
   offset = pio_add_program(pio, &read_addr_program);
-  pio_sm_config c = read_addr_program_get_default_config(offset);
-  sm_config_set_in_pins(&c, in_pin_base);
-  sm_config_set_out_pins(&c, out_pin_base, 5);
-  pio_sm_set_consecutive_pindirs(pio, sm, in_pin_base, 8, false);
-  pio_sm_set_consecutive_pindirs(pio, sm, in_pin_base, 5, true);
+  c = read_addr_program_get_default_config(offset);
+  sm_config_set_in_pins(&c, muxed_cartridge_signals);
+  sm_config_set_set_pins(&c, mux_control, 4);
+  sm_config_set_in_shift(&c, false,  // shift_direction=left
+                         true, 15);  // autopush enabled
   pio_sm_init(pio, sm, offset, &c);
   pio_sm_set_enabled(pio, sm, true);
 
-  // TODO: Configure data state machine
+  // Configure data state machine
+  uint data_out_sm = pio_claim_unused_sm(pio, true);
+  offset = pio_add_program(pio, &data_out_program);
+  c = data_out_program_get_default_config(offset);
+  sm_config_set_out_pins(&c, muxed_cartridge_signals, 8);
+  sm_config_set_in_pins(&c, muxed_cartridge_signals + 8);
+  sm_config_set_sideset_pins(&c, mux_control);
+  sm_config_set_in_shift(&c, false,   // shift_direction=left
+                         false, 32);  // autopush disabled
+
+  // TODO: configure GPIO
 
   // DMA chain to connect the two state machines
-  init_dma(pio_get_dreq(pio, sm, false), &pio->rxf[sm], NULL, rom_addr);
+  init_dma(pio_get_dreq(pio, read_addr_sm, false), &pio->rxf[read_addr_sm],
+           &pio->txf[data_out_sm], rom_addr);
+
+  // TODO: enable SMs
+}
 }
 
 int main() {
